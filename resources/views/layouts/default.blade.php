344bc79e586bb77d095aae85528252a046f2ea2d
Barsnes <tobiasbarsnes@gmail.com>
2018-12-28 11:36:25 -0600

    Fontawesome + Footer

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{ asset('js/app.js') }}" defer></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{ Html::style('css/fontawesome/css/all.css') }}
     @yield('script')
   </head>
   <body>
@@ -78,7 +79,136 @@
   </div>
   </nav>
 
+<div style="min-height:100vh">
     @yield('content')
+</div>
 
+    <section id="footer">
+  		<div class="container pt-5">
+  			<div class="row text-center text-xs-center text-sm-left text-md-left">
+  				<div class="col-xs-12 col-sm-4 col-md-4">
+  					<h5>Quick links</h5>
+  					<ul class="list-unstyled quick-links">
+  						<li><a href="/"><i class="fa fa-angle-double-right"></i>Home</a></li>
+  						<li><a href="/browse"><i class="fa fa-angle-double-right"></i>Broswe</a></li>
+  						<li><a href="/faq"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+  						<li><a href="/register"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+  						<li><a href="/browse"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+  					</ul>
+  				</div>
+  				<div class="col-xs-12 col-sm-4 col-md-4">
+  					<h5>Quick links</h5>
+  					<ul class="list-unstyled quick-links">
+  						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
+  						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
+  						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+  						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+  						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+  					</ul>
+  				</div>
+  				<div class="col-xs-12 col-sm-4 col-md-4">
+  					<h5>Quick links</h5>
+  					<ul class="list-unstyled quick-links">
+  						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
+  						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
+  						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+  						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+  						<li><a href="https://wwwe.sunlimetech.com" title="Design and developed by"><i class="fa fa-angle-double-right"></i>Imprint</a></li>
+  					</ul>
+  				</div>
+  			</div>
+  			<div class="row">
+  				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+  					<ul class="list-unstyled list-inline social text-center">
+  						{{-- <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-facebook"></i></a></li> --}}
+  						<li class="list-inline-item"><a href="https://twitter.com/_Barsnes" target="_blank"><i class="fab fa-twitter"></i></a></li>
+  						<li class="list-inline-item"><a href="https://github.com/Barsnes/Go-Smoke.me" target="_blank"><i class="fab fa-github"></i></a></li>
+  						{{-- <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-google-plus"></i></a></li> --}}
+  						<li class="list-inline-item"><a href="mailto:tobiasbarsnes@gmail.com" target="_blank"><i class="fa fa-envelope"></i></a></li>
+  					</ul>
+  				</div>
+  				</hr>
+  			</div>
+  			<div class="row">
+  				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+  					<p class="h6">&copy All right Reversed.<a class="text-green ml-2" href="https://go-smoke.me" target="_blank">GoSmoke.ME</a></p>
+  				</div>
+  				</hr>
+  			</div>
+  		</div>
+  	</section>
   </body>
+
+  <style media="screen">
+  section .section-title {
+  text-align: center;
+  color: #007b5e;
+  margin-bottom: 50px;
+  text-transform: uppercase;
+}
+#footer {
+  background: #ECA72C !important;
+}
+#footer h5{
+padding-left: 10px;
+  border-left: 3px solid #eeeeee;
+  padding-bottom: 6px;
+  margin-bottom: 20px;
+  color:#ffffff;
+}
+#footer a {
+  color: #ffffff;
+  text-decoration: none !important;
+  background-color: transparent;
+  -webkit-text-decoration-skip: objects;
+}
+#footer ul.social li{
+padding: 3px 0;
+}
+#footer ul.social li a i {
+  margin-right: 5px;
+font-size:25px;
+-webkit-transition: .5s all ease;
+-moz-transition: .5s all ease;
+transition: .5s all ease;
+}
+#footer ul.social li:hover a i {
+font-size:30px;
+margin-top:-10px;
+}
+#footer ul.social li a,
+#footer ul.quick-links li a{
+color:#ffffff;
+}
+#footer ul.social li a:hover{
+color:#eeeeee;
+}
+#footer ul.quick-links li{
+padding: 3px 0;
+-webkit-transition: .5s all ease;
+-moz-transition: .5s all ease;
+transition: .5s all ease;
+}
+#footer ul.quick-links li:hover{
+padding: 3px 0;
+margin-left:5px;
+font-weight:700;
+}
+#footer ul.quick-links li a i{
+margin-right: 5px;
+}
+#footer ul.quick-links li:hover a i {
+  font-weight: 700;
+}
+
+@media (max-width:767px){
+#footer h5 {
+  padding-left: 0;
+  border-left: transparent;
+  padding-bottom: 0px;
+  margin-bottom: 10px;
+}
+}
+
+  </style>
 </html>
